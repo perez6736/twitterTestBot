a02d0147837a3f673042919f94ca2107089842d7
Danny Perez <daniel.perez@structuredweb.com>
2017-09-13 14:50:03 -0400

    added instructions to setup the config file

--- configEmpty.js
+++ configEmpty.js
@@ -5,3 +5,10 @@ module.exports = {
   access_token_secret:  '...',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
+
+// Instructions ----------
+// Go to dev.twitter.com -> manage your apps and follow the instructions there 
+// to get the keys needed. 
+
+// Once you have the keys paste them in the correct key in the object above
+//be sure to rename this file to config.js when you are done. 
