bd2dc89fee1ec78bbe6f00d059349e81de3a4268
perez6736 <perez6736@gmail.com>
2017-09-16 13:59:11 -0400

    added moment.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 console.log("Bot is starting.");
 
 var Twit = require('twit');
+var moment = require('moment');
 var config = require('./config');
 
 var T = new Twit(config);
@@ -14,21 +15,7 @@ T.post('statuses/update', tweet, tweeted);
 
 // returns an integer that tells you how many days until xmas
 function calculateDaysUntilXmas(){
-			var d = new Date();
-		    var minutes = 1000*60;
-            var hours = minutes*60;
-            var days = hours*24;
-            var year = d.year;
 
-            //if(todays date is between 12/26 - 12/31)
-            	//year++
-
-            //todays date
-            var todaysDate = 
-            var christmasDay = getDateFromFormat("12/25/" + year, "M/d/y");
-
-            var diff_date = Math.round((christmasDay - todaysDate)/days);
-            alert("Diff date is: " + diff_date );
 }
 
 
