66cd5caae1d8a8d053601ec79f4e3b4ee9a59ceb
perez6736 <perez6736@gmail.com>
2017-09-30 10:48:52 -0400

    heroku config keys

--- bot.js
+++ bot.js
@@ -2,7 +2,13 @@ console.log("Bot is starting.");
 
 var Twit = require('twit');
 var moment = require('moment');
-var config = require('./config');
+var config = {
+  consumer_key:         process.env.consumer_key,
+  consumer_secret:      process.env.consumer_secret,
+  access_token:         process.env.access_token,
+  access_token_secret:  process.env.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
 
 var T = new Twit(config);
 
